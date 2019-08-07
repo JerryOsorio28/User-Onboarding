48e840bb2ca3d3077c7b2be5e65123de189ef60b
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-08-07 18:05:05 -0400

    WIP User Onboarding Project

---
+++ user-onboarding/src/components/UserList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserList = (users) =>{
+    console.log(users.users)
+    return(
+        <div>
+            {/* {props.props.map(follower => <FollowersCard key={follower.id}>{follower.login}</FollowersCard>)} */}
+        </div>
+    )   
+};
+
+export default UserList;
